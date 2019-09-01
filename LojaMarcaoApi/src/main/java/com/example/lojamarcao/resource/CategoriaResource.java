afa92f8aec5fe4e82420f7becdacb9d00402409f
Mauri Vinicius <viniciusmauri@outlook.com.br>
2019-09-01 19:19:53 -0300

    Adicionando função de criar categoria e busca pelo código

--- LojaMarcaoApi/src/main/java/com/example/lojamarcao/resource/CategoriaResource.java
+++ LojaMarcaoApi/src/main/java/com/example/lojamarcao/resource/CategoriaResource.java
@@ -1,11 +1,21 @@
 package com.example.lojamarcao.resource;
 
+import java.net.URI;
 import java.util.List;
 
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.lojamarcao.model.Categoria;
 import com.example.lojamarcao.repository.CategoriaRepository;
@@ -21,5 +31,23 @@ public class CategoriaResource {
 	public List<Categoria> listar() {
 		return categoriaRepository.findAll();
 
+	}
+
+	@PostMapping
+	@ResponseStatus(HttpStatus.CREATED)
+	public ResponseEntity<Categoria> criarCategoria(@RequestBody Categoria categoria, HttpServletResponse response) {
+		Categoria categoriaSalva = categoriaRepository.save(categoria);
+
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequestUri().path("/{cod}")
+				.buildAndExpand(categoriaSalva.getCod())
+				.toUri();
+		response.setHeader("Location", uri.toASCIIString());
+
+		return ResponseEntity.created(uri).body(categoriaSalva);
+	}
+
+	@GetMapping("/{cod}")
+	public Categoria buscarPeloCod(@PathVariable Long cod) {
+		return categoriaRepository.findById(cod).orElse(null);
 	}
 }
