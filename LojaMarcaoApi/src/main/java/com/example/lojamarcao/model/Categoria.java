04fb881502c7b1d3220e805a03a94fead3de158d
Mauri Vinicius <viniciusmauri@outlook.com.br>
2019-08-31 23:12:30 -0300

    Iniciando Projeto Loja do Marcão

---
+++ LojaMarcaoApi/src/main/java/com/example/lojamarcao/model/Categoria.java
@@ -0,0 +1,59 @@
+package com.example.lojamarcao.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "categoria")
+public class Categoria {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long cod;
+
+	private String nome;
+
+	public Long getCod() {
+		return cod;
+	}
+
+	public void setCod(Long cod) {
+		this.cod = cod;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((cod == null) ? 0 : cod.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Categoria other = (Categoria) obj;
+		if (cod == null) {
+			if (other.cod != null)
+				return false;
+		} else if (!cod.equals(other.cod))
+			return false;
+		return true;
+	}
+}
---
+++ LojaMarcaoApi/src/main/java/com/example/lojamarcao/repository/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.example.lojamarcao.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.example.lojamarcao.model.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+
+}
