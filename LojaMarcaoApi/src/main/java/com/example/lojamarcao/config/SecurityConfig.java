a5ca9773b4f2838dd88e3431d53f2feec66a98f5
Mauri Vinicius <viniciusmauri@outlook.com.br>
2019-10-12 10:04:27 -0300

    Iniciando a implementação de segurança na api

--- LojaMarcaoApi/src/main/java/com/example/lojamarcao/config/SecurityConfig.java
+++ LojaMarcaoApi/src/main/java/com/example/lojamarcao/config/SecurityConfig.java
@@ -1,5 +1,8 @@
 package com.example.lojamarcao.config;
 
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
