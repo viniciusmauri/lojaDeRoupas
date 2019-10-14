7ca280b875b7652a5381677e8674a21e66362b78
Mauri Vinicius <viniciusmauri@outlook.com.br>
2019-10-13 21:49:27 -0300

    Adicionando Autorização de acesso na API

--- LojaMarcaoApi/src/main/java/com/example/lojamarcao/config/SecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package com.example.lojamarcao.config;
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-
-}
